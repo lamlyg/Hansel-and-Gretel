3cd13930ac453a3a6aa89b7a6bf3c402d488d6e7
lamlyg <lamlyg@naver.com>
2020-06-09 22:37:30 +0900

    [상품 상세보기 페이지] 상품 상세보기 기능

---
+++ Hansel-and-Gretel/client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Descriptions } from 'antd';
+
+function ProductInfo(props) {
+
+    const [Product, setProduct] = useState({})
+
+    useEffect(() => {
+
+        setProduct(props.detail)
+
+    }, [props.detail])
+
+
+
+    return (
+        <div>
+            <Descriptions title="Product Info">
+                <Descriptions.Item label="Price"> {Product.price}</Descriptions.Item>
+                <Descriptions.Item label="Sold">{Product.sold}</Descriptions.Item>
+                <Descriptions.Item label="View"> {Product.views}</Descriptions.Item>
+                <Descriptions.Item label="Description"> {Product.description}</Descriptions.Item>
+            </Descriptions>
+
+            <br />
+            <br />
+            <br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button size="large" shape="round" type="danger"
+                    
+                >
+                    장바구니에 담기
+                    </Button>
+            </div>
+        </div>
+    )
+}
+
+export default ProductInfo
