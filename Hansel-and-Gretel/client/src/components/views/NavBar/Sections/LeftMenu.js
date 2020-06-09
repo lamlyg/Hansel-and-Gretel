828438f44d13b5d7d65008eabaaeded98b9d19ed
lamlyg <lamlyg@naver.com>
2020-06-09 23:22:15 +0900

    버그 해결 / 디자인 다듬기 등의 보완

--- Hansel-and-Gretel/client/src/components/views/NavBar/Sections/LeftMenu.js
+++ Hansel-and-Gretel/client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -7,12 +7,12 @@ function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
     <Menu.Item key="mail">
-      <a href="/">Home</a>
+      <a href="/">YUMMY</a>
     </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
+    <SubMenu title={<span>CATEGORY</span>}>
       <MenuItemGroup title="Item 1">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
+        <Menu.Item key="setting:1">스낵류</Menu.Item>
+        <Menu.Item key="setting:2">캔디류</Menu.Item>
       </MenuItemGroup>
       <MenuItemGroup title="Item 2">
         <Menu.Item key="setting:3">Option 3</Menu.Item>
